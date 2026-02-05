aa46b4b4371db71bb1739335794bc1e412f74294
senacbenjamin <senacbenjamin@gmail.com>
2026-02-04 21:55:43 -0300

    feat : adicionar camada de aplicaçao

---
+++ backend/MeuCorre.Application/UseCases/Tags/AtualizarTagCommand.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuCorre.Application.UseCases.Tags
+{
+    public class AtualizarTagcommand : IRequest<(string, bool)>
+
+    {
+        [Required(ErrorMessage = "Id do usuario é obrigatorio")]
+
+        public required Guid UsuarioId { get; set; }
+
+        [Required(ErrorMessage = "Nome é obrigatorio")]
+
+        public required string Nome { get; set; }
+
+        [Required(ErrorMessage = "A cor é obrigatorio")]
+        []
+
+        public string cor { get; private set; }
+    }
+    internal class AtualizarTagCommandHandler : IRequestHandler<AtualizarTagcommand, (string, bool)>
+    {
+        public Task<(string, bool)> Handle(AtualizarTagcommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
---
+++ backend/MeuCorre.Application/UseCases/Tags/CriarTagcommand.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuCorre.Application.UseCases.Tags
+{
+     public class CriarTagcommand : IRequest<(string, bool)>
+
+    {
+        [Required(ErrorMessage = "Id do usuario é obrigatorio")]
+
+        public required Guid UsuarioId { get; set; }
+
+        [Required(ErrorMessage = "Nome é obrigatorio")]
+
+        public required string Nome { get; set; }
+
+        [Required(ErrorMessage = "A cor é obrigatorio")]
+
+        public string cor { get; private set; }
+    }
+    internal class CriarTagCommandHandler : IRequestHandler<CriarTagcommand, (string, bool)>
+    {
+        public Task<(string, bool)> Handle(CriarTagcommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
